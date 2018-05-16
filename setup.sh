c0b32292f15c9a2447cf4526227b88a3197a2584
RyanSquared <vandor2012@gmail.com>
2018-05-16 15:50:21 -0500

    setup: Use Terraform

--- setup.sh
+++ setup.sh
@@ -3,23 +3,12 @@
 # Don't set -eu in case policies/resources already exist
 set -x
 
-REGION="${REGION:-us-east-2}"
-BUCKET="${BUCKET:-noticast-messages}"
+# zip up the lambda
+if test ! -f message_lambda.zip; then
+  pushd vendor/message-lambda
+  zip -r ../../message_lambda.zip *
+  popd
+fi
 
-for policy in $(ls iam/policies); do
-  aws iam create-policy --policy-name "${policy%.*}" --policy-document file://iam/policies/$policy
-done
-
-for policy in $(ls iot/policies); do
-  aws iot create-policy --policy-name "${policy%.*}" --policy-document file://iot/policies/$policy
-done
-
-# used by devices to know when to pull a message
-# message payload format:
-# `message_text:message_time:message_mp3_filename`
-# use `message.split(":")` to get a list in Python
-aws sns create-topic --name play-message
-
-# write permission from the lambda to put MP3s into
-# read permission from the devices to pull MP3s from
-aws s3api create-bucket --bucket "$BUCKET" --region "$REGION" --create-bucket-configuration LocationConstraint="$REGION"
+terraform plan -out=terraform.apply
+echo "Run \`terraform apply terraform.apply\` to set up infrastructure."

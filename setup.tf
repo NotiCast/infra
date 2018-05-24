e073d3f2073cdce6f585cd23e36f69e7b1950dbd
RyanSquared <vandor2012@gmail.com>
2018-05-24 16:34:18 -0500

    setup.{tf,sh},pre/dns.tf: DNS and TLS setup

--- setup.tf
+++ setup.tf
@@ -7,6 +7,16 @@ variable "bucket_name" {
   default = "noticast-messages"
 }
 
+variable "root_name" {
+  type = "string"
+  default = "notica.st"
+}
+
+variable "domain_name" {
+  type = "string"
+  default = "api.notica.st"
+}
+
 # This will need to be changed per-deployment
 terraform {
   backend "s3" {
@@ -17,17 +27,31 @@ terraform {
 
 # REST API access point
 
+provider "aws" {
+  region = "us-east-1"
+  alias = "edge"
+}
+
 resource "aws_api_gateway_rest_api" "messages-api" {
+  provider = "aws.edge"
   name = "messages-api"
+
+  endpoint_configuration = {
+    types = ["EDGE"]
+  }
 }
 
 resource "aws_api_gateway_resource" "messages-endpoint" {
+  provider = "aws.edge"
+
   rest_api_id = "${aws_api_gateway_rest_api.messages-api.id}"
   parent_id = "${aws_api_gateway_rest_api.messages-api.root_resource_id}"
   path_part = "send_message"
 }
 
 resource "aws_api_gateway_method" "messages-endpoint-method" {
+  provider = "aws.edge"
+
   rest_api_id = "${aws_api_gateway_rest_api.messages-api.id}"
   resource_id = "${aws_api_gateway_resource.messages-endpoint.id}"
   http_method = "POST"
@@ -35,12 +59,69 @@ resource "aws_api_gateway_method" "messages-endpoint-method" {
 }
 
 resource "aws_api_gateway_integration" "messages-lambda" {
+  provider = "aws.edge"
+
   rest_api_id = "${aws_api_gateway_rest_api.messages-api.id}"
   resource_id = "${aws_api_gateway_resource.messages-endpoint.id}"
   http_method = "${aws_api_gateway_method.messages-endpoint-method.http_method}"
   integration_http_method = "POST"
   type = "AWS_PROXY"
   uri = "${aws_lambda_function.message-lambda.invoke_arn}"
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "${var.root_name}"
+}
+
+resource "aws_route53_record" "messages-api" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "${aws_api_gateway_domain_name.messages-api.domain_name}"
+  type = "A"
+
+  alias {
+    name = "${aws_api_gateway_domain_name.messages-api.cloudfront_domain_name}"
+    zone_id = "${aws_api_gateway_domain_name.messages-api.cloudfront_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "messages-api-cert-validation" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "${aws_acm_certificate.messages-api.domain_validation_options.0.resource_record_name}"
+  type = "${aws_acm_certificate.messages-api.domain_validation_options.0.resource_record_type}"
+  records = ["${aws_acm_certificate.messages-api.domain_validation_options.0.resource_record_value}"]
+  ttl = 60
+}
+
+resource "aws_acm_certificate" "messages-api" {
+  provider = "aws.edge"
+
+  domain_name = "${var.domain_name}"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "messages-api" {
+  provider = "aws.edge"
+
+  certificate_arn = "${aws_acm_certificate.messages-api.arn}"
+  validation_record_fqdns = ["${aws_route53_record.messages-api-cert-validation.fqdn}"]
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name = "${aws_acm_certificate.messages-api.domain_validation_options.0.resource_record_name}"
+  type = "${aws_acm_certificate.messages-api.domain_validation_options.0.resource_record_type}"
+  zone_id = "${aws_route53_zone.primary.id}"
+  records = ["${aws_acm_certificate.messages-api.domain_validation_options.0.resource_record_value}"]
+  ttl = 60
+}
+
+resource "aws_api_gateway_domain_name" "messages-api" {
+  domain_name = "${var.domain_name}"
+
+  certificate_arn = "${aws_acm_certificate.messages-api.arn}"
+  endpoint_configuration = {
+    types = ["EDGE"]
+  }
 }
 
 resource "aws_lambda_permission" "messages-lambda" {

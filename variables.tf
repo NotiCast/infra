04ff5c67a9320d739ac931d2388bb62fb2b5fee8
RyanSquared <vandor2012@gmail.com>
2018-08-08 12:59:29 -0500

    variables.tf: Add subnet config

--- variables.tf
+++ variables.tf
@@ -26,3 +26,23 @@ variable "noticast_web_stage" {
   type = "string"
   default = "development"
 }
+
+variable "subnet_azs" {
+  type = "list"
+  default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+
+variable "vpc_cidr" {
+  type = "string"
+  default = "172.31.0.0/16"
+}
+
+variable "private_subnet_cidr" {
+  type = "string"
+  default = "172.31.48.0/20"
+}
+
+variable "private_subnet_ips" {
+  type = "list"
+  default = ["172.31.17.161"]  # who knows that's what they gave me
+}

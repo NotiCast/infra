70a3f77f96909d44d467babc77418c21a1769eb8
RyanSquared <vandor2012@gmail.com>
2018-08-20 15:02:02 -0500

    email.tf: Initial Commit

---
+++ email.tf
@@ -0,0 +1,54 @@
+resource "aws_ses_domain_identity" "primary" {
+  provider = "aws.edge"  # Not available in us-east-2
+
+  domain = "${var.incoming_email_subdomain}.${var.domain_name}"
+}
+
+resource "aws_route53_record" "ses-verification" {
+  zone_id = "${aws_route53_zone.primary.id}"
+  name = "_amazonses.${var.incoming_email_subdomain}.${var.domain_name}"
+  type = "TXT"
+  ttl = "600"
+  records = ["${aws_ses_domain_identity.primary.verification_token}"]
+}
+
+resource "aws_route53_record" "ses-incoming" {
+  zone_id = "${aws_route53_zone.primary.id}"
+  name = "${var.incoming_email_subdomain}.${var.domain_name}"
+  type = "MX"
+  ttl = "60"
+  records = ["1 INBOUND-SMTP.US-EAST-1.AMAZONAWS.COM"]
+}
+
+resource "aws_route53_record" "ses-spf" {
+  zone_id = "${aws_route53_zone.primary.id}"
+  name = "@"
+  type = "TXT"
+  ttl = "60"
+  records = ["v=spf1 include:amazonecs.com ~all"]
+}
+
+resource "aws_ses_receipt_rule_set" "default" {
+  provider = "aws.edge"
+  rule_set_name = "default-rule-set"
+}
+
+resource "aws_ses_active_receipt_rule_set" "default" {
+  provider = "aws.edge"
+  rule_set_name = "${aws_ses_receipt_rule_set.default.rule_set_name}"
+}
+
+resource "aws_ses_receipt_rule" "fallback" {
+  provider = "aws.edge"
+  name = "mailgun"
+  rule_set_name = "${aws_ses_receipt_rule_set.default.rule_set_name}"
+  recipients = ["send.noticast.io"]
+  enabled = true
+
+  lambda_action {
+    function_arn = "${aws_lambda_function.message-lambda.arn}"
+    position = "1"
+  }
+
+  depends_on = ["aws_lambda_permission.ses-lambda-permission"]
+}
